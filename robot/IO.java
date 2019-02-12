be6983ade63f41ef6cc496709e05283e8f857dde
Andrew Ge <42222478+andrewdge@users.noreply.github.com>
2019-02-11 22:29:46 -0500

    just before pneumatics

---
+++ robot/IO.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.SerialPort.Port;
+
+public final class IO{
+    private IO(){}
+
+    //CAN
+    public static final int motorDriveLeftTop = 1;
+    public static final int motorDriveLeftBottom = 2;
+    public static final int motorDriveRightTop = 3;
+    public static final int motorDriveRightBottom = 4;
+    
+    //Solenoid
+    public static final int ballOut = 0;
+    public static final int ballIn = 1;
+
+    //PWM
+    
+    
+
+    //analog
+    
+
+    //digital
+    public static final int limitSwitchElevatorBottom = 0;
+    public static final int limitSwitchElevatorTop = 1;
+    public static final int encoderDriveLeftA = 2;
+    public static final int encoderDriveLeftB = 3;
+    public static final int encoderDriveRightA = 4;
+    public static final int encoderDriveRightB = 5;
+
+    //USB
+    public static final SerialPort.Port gyro = SerialPort.Port.kUSB;
+
+}
